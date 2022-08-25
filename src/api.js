15555be106d68f0a39cd33535f14666f18866a7a
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-25 19:08:24 -0300

    feat: middleware de tratamento de erros lançados

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
-// ...
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
+app.use((err, _req, res, _next) => {
+  res.status(err.code || 500).json({ message: err.message });
+});
 
 // ...
 
