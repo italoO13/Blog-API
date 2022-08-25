4d9481f53705b922072906bc31cba2e0b9e0d664
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-25 20:50:36 -0300

    feat: adicionado o methodo get na rota user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
 const userValidate = require('../middlewares/user.validators');
+const { authenticated } = require('../middlewares/auth.validors');
 
 const router = express.Router();
 
 router.post('/', userValidate.create, userController.create);
+router.get('/', authenticated, userController.getAll);
 
 module.exports = router;
