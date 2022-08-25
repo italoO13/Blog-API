71b5711b4dcef8515b7da57a989d7ea8324686d3
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-25 19:58:40 -0300

    feat: inserido rota user nas rotas do express

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const loginRouter = require('./login.routes');
+const userRouter = require('./user.routes');
 
 const routes = express.Router();
 
 routes.use('/login', loginRouter);
+routes.use('/user', userRouter);
 
 module.exports = routes;
