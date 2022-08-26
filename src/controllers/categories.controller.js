2c00e5029abf267c90e45912a7eabb962d201471
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-26 09:29:47 -0300

    feat: camada de controller para o retorno de todas as categorias

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -10,6 +10,17 @@ const create = async (req, res, next) => {
   }
 };
 
+const getAll = async (req, res, next) => {
+  try {
+    const response = await CategoriesService.getAll();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
